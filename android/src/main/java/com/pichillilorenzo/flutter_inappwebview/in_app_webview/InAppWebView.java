229be05d68bd271a06badff52fb96d3dc2d139d8
MC Koo <koomingchin@gmail.com>
2022-08-10 05:58:45 +0800

    Added full page screenshot for android

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/in_app_webview/InAppWebView.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/in_app_webview/InAppWebView.java
@@ -577,10 +577,7 @@ final public class InAppWebView extends InputAwareWebView implements InAppWebVie
       @Override
       public void run() {
         try {
-          int contentHeight = getMeasuredHeight();
-          //if (screenshotConfiguration == null) {
-            contentHeight = (int) Math.floor(getContentHeight() * pixelDensity);
-          //}
+          int contentHeight = (int) Math.floor(getContentHeight() * pixelDensity);
 
           Bitmap screenshotBitmap = Bitmap.createBitmap(getMeasuredWidth(), contentHeight, Bitmap.Config.ARGB_8888);
           Canvas c = new Canvas(screenshotBitmap);
